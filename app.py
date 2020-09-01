83ebd654d7ec7cbae3469fe52cd800487ce8b1be
Erickson Lopes <erickson-lopes-dev>
2020-09-01 15:27:53 -0300

    monstando rotas

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return {'Olá mundo': 'message0'}, 200
+@app.route('/<name>/<int:id>/', methods=['GET', 'POST'])
+def main(name, id):
+    print(request.data)
+    return f'hello {name}, com o id {id}', 200
 
+
+# app.add_url_rule('/', 'hello', main)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
