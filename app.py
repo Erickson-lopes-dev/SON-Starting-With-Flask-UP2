a561b24fa241346dce03d43cf86761f0a6a136d2
EricksonLopes <ofc.erickson@gmail.com>
2020-09-01 21:31:23 -0300

    Provendo arquivos estáticos

--- app.py
+++ app.py
@@ -53,5 +53,10 @@ def result(name):
     return render_template('index.html', name=name)
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', error=error), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
